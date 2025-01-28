8d1501e1312d198096b943926d5c5b4034643fe2
matteocaschetto <matteocaschetto9@gmail.com>
2025-01-28 11:52:28 +0100

    FIRST COMMIT MATTEO

---
+++ src/main/java/org/example/entities/Autobus.java
@@ -0,0 +1,7 @@
+package org.example.entities;
+
+public class Autobus extends Mezzo{
+    public Autobus(String tipoMezzo, int capienzaMezzo, boolean inServizio){
+        super("autobus", capienzaMezzo, inServizio);
+    }
+}
